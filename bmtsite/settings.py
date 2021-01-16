bda1b90926296eaf1999a4862fdc1b61e0585805
mascher <kurt@singlecomm.com>
2021-01-16 14:32:25 -0500

    Update settings.py

--- bmtsite/settings.py
+++ bmtsite/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+DISABLE_COLLECTSTATIC=1
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
