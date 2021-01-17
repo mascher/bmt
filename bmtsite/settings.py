9891183b417272f6ebcc8e23cef7ba709f0ab9ce
mascher <kurt@singlecomm.com>
2021-01-17 13:49:19 -0500

    Update settings.py

--- bmtsite/settings.py
+++ bmtsite/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-
+DISABLE_COLLECTSTATIC=1
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-DISABLE_COLLECTSTATIC=1
+
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
